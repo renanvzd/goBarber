8900f31e32b68ef82602f0b5074dbad1b685e2da
Renan Veronez Drechsler <renanvz@outlook.com>
2020-11-25 23:33:21 -0300

    Relacionamento nos Models

--- back-end/src/database/migrations/1606052876074-CreateAppoinments.ts
+++ back-end/src/database/migrations/1606052876074-CreateAppoinments.ts
@@ -9,7 +9,7 @@ export default class CreateAppoinments1606052876074
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- back-end/src/database/migrations/1606175630221-CreateUser.ts
+++ back-end/src/database/migrations/1606175630221-CreateUser.ts
@@ -8,7 +8,7 @@ export default class CreateUser1606175630221 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
