e3654f54f00297389e6d442bb72ee30350cade93
Renan Veronez Drechsler <renanvz@outlook.com>
2020-11-29 19:17:53 -0300

    Rotas autenticadas - Middlewares

---
+++ back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'dcac3e07a13c0f1c4c8b6062124131b2',
+    expiresIn: '1d',
+  },
+};
--- back-end/src/services/AuthenticateUserService.ts
+++ back-end/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -33,9 +34,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, 'dcac3e07a13c0f1c4c8b6062124131b2', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
