c0eeacbb822ba7d57eeb3eb0c71e153861dfae2e
Renan Veronez Drechsler <renanvz@outlook.com>
2021-01-29 00:15:35 -0300

    Reescrevendo Repositorios

--- back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
+import ICReateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
+  create(data: ICReateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
