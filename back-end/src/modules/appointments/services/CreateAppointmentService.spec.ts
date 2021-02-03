d045a6c3049c8c6a16744493e9f150acf8a9d837
Renan Veronez Drechsler <renanvz@outlook.com>
2021-02-03 19:55:47 -0300

    Pensando nos testes

--- back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,11 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect();
+  });
+
+  // it('should not be able to create a new appointment on the same time', () => {
+  //   expect()
+  // });
 });
