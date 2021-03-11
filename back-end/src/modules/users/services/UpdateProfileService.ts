06f18f681836e9fba55f96f3960ec166a4822b27
Renan Veronez Drechsler <renanvz@outlook.com>
2021-03-11 00:43:32 -0300

    Rota e Controller de perfil

--- back-end/src/modules/users/services/UpdateProfileService.ts
+++ back-end/src/modules/users/services/UpdateProfileService.ts
@@ -15,7 +15,7 @@ interface IRequest {
 }
 
 @injectable()
-class UpdateProfile {
+class UpdateProfileService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
@@ -68,4 +68,4 @@ class UpdateProfile {
     return this.usersRepository.save(user);
   }
 }
-export default UpdateProfile;
+export default UpdateProfileService;
