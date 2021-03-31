c598d9cea0a04ef03b23277cc7cfeafe55b7ee55
Renan Veronez Drechsler <renanvz@outlook.com>
2021-03-31 20:33:23 -0300

    E-mails pela Amazon SES - Ajustes

--- back-end/src/config/mail.ts
+++ back-end/src/config/mail.ts
@@ -10,7 +10,7 @@ interface IMailConfig {
 }
 
 export default {
-  driver: process.env.MAIL_DRIVER || 'ethereal',
+  driver: `${process.env.MAIL_DRIVER}` || 'ethereal',
 
   defaults: {
     from: {
