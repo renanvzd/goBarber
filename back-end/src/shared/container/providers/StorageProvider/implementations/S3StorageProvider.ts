2c5cbdbbee2707ae56f2719173f099ebf33835b8
Renan Veronez Drechsler <renanvz@outlook.com>
2021-04-04 00:00:36 -0300

    Upload Arquivos Amazon S3 - Finalizado

--- back-end/src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ back-end/src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import mime from 'mime';
 import aws, { S3 } from 'aws-sdk';
 import uploadConfig from '@config/upload';
 
@@ -17,18 +18,25 @@ class DiskStorageProvider implements IStorageProvider {
   public async saveFile(file: string): Promise<string> {
     const originalPath = path.resolve(uploadConfig.tmpFolder, file);
 
-    const fileContent = await fs.promises.readFile(originalPath, {
-      encoding: 'utf-8',
-    });
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new Error('File not found');
+    }
+
+    const fileContent = await fs.promises.readFile(originalPath);
 
     await this.client
       .putObject({
-        Bucket: 'app-gobarber-renan',
+        Bucket: uploadConfig.config.aws.bucket,
         Key: file,
         ACL: 'public-read',
         Body: fileContent,
+        ContentType,
       })
       .promise();
+
+    await fs.promises.unlink(originalPath);
 
     return file;
   }
@@ -36,7 +44,7 @@ class DiskStorageProvider implements IStorageProvider {
   public async deleteFile(file: string): Promise<void> {
     await this.client
       .deleteObject({
-        Bucket: 'app-gobarber-renan',
+        Bucket: uploadConfig.config.aws.bucket,
         Key: file,
       })
       .promise();
